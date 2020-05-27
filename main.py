fix normalization option in main.py

main.py
@@ -34,59 +34,64 @@ from keras.preprocessing.image import ImageDataGenerator
 
 
 def mymkdir(path):
-	if not os.path.exists(path):
-		os.mkdir(path)
+    if not os.path.exists(path):
+        os.mkdir(path)
 
 # Learning function of the U-NET model
 
 def Training_Unet(mainPath='',Augmentation=True,nb_Augmentation=3,INPUT_WIDTH = 256,INPUT_HEIGHT = 256, INPUT_CHANNELS = 7, OUTPUT_CHANNELS=1, 
-	Normalization ="Normalizing_by_image_by_column",Train_model=True, Load_weights=True,validation_split=0.1, batch_size = 5, epochs=30):
-	PathTT=mainPath+'data/'
+    Normalization ="Normalizing_by_image_by_column",Train_model=True, Load_weights=True,validation_split=0.1, batch_size = 5, epochs=30):
+    PathTT=mainPath+'data/'
 
-	EVALUATION_PATH=mainPath+"/EVALUATIONS/"+str(INPUT_WIDTH)+'_'+str(INPUT_CHANNELS)+'_'+ str(Augmentation)+'_'+str(nb_Augmentation)+'_'+str(Normalization)+'_'+str(batch_size)+'_'+str(epochs)
+    EVALUATION_PATH=mainPath+"/EVALUATIONS/"+str(INPUT_WIDTH)+'_'+str(INPUT_CHANNELS)+'_'+ str(Augmentation)+'_'+str(nb_Augmentation)+'_'+str(Normalization)+'_'+str(batch_size)+'_'+str(epochs)
 
-	# Creation of the log directory 
-	mymkdir(mainPath+"/EVALUATIONS")
-	mymkdir(EVALUATION_PATH)
+    # Creation of the log directory 
+    mymkdir(mainPath+"/EVALUATIONS")
+    mymkdir(EVALUATION_PATH)
 
-	# Loading data training and test data
-	size_image_input=[INPUT_WIDTH ,INPUT_HEIGHT,INPUT_CHANNELS]
-	size_image_output=[INPUT_WIDTH ,INPUT_HEIGHT,OUTPUT_CHANNELS]
+    # Loading data training and test data
+    size_image_input=[INPUT_WIDTH ,INPUT_HEIGHT,INPUT_CHANNELS]
+    size_image_output=[INPUT_WIDTH ,INPUT_HEIGHT,OUTPUT_CHANNELS]
 
-	X_Train,Y_Train,X_Test,Y_Test=load_train_test_data.load_training_test_data(mainPath=mainPath, size_image_Input=size_image_input,
-	 size_mask_Output=size_image_output,augment=Augmentation, nb_augment=nb_Augmentation)
+    X_Train,Y_Train,X_Test,Y_Test=load_train_test_data.load_training_test_data(mainPath=mainPath, size_image_Input=size_image_input,
+     size_mask_Output=size_image_output,augment=Augmentation, nb_augment=nb_Augmentation)
 
-	#Check the max and min of each column of input data, it must equal to (1,1,1,1,1,1) and (0 0 0 0 0 0) respectively.
+    #Check the max and min of each column of input data, it must equal to (1,1,1,1,1,1) and (0 0 0 0 0 0) respectively.
     print("Initial data")
     print([np.max(X_Train[:,:,i]) for i in range(7)])
     print([np.min(X_Train[:,:,i]) for i in range(7)])
 
-	# Normalizing data
-	# Initializing vectors
-	X_Train_Normal=np.zeros_like(X_Train)
-	X_Test_Normal=np.zeros_like(X_Test)
+    # Normalizing data
+    # Initializing vectors
+    X_Train_Normal=np.zeros_like(X_Train)
+    X_Test_Normal=np.zeros_like(X_Test)
 
-	X_Train_Normal=data_normalizing.Normalizing_by_image_by_column(X_Train)
-	X_Test_Normal=data_normalizing.Normalizing_by_image_by_column(X_Test)
+
+    if not Normalization is None:
+       normfunc =  data_normalizing.__dict__[Normalization]
+       X_Train_Normal=normfunc(X_Train)
+       X_Test_Normal=normfunc(X_Test)
+    elif 
+       X_Train_Normal=X_Train
+       X_Test_Normal=X_Test
+
     print("the shape of X_Train is  ",np.shape(X_Train))
     print("the shape of normalized X_Train is  ",np.shape(X_Train_Normal))
-	print("the shape of Y_Train is  ",np.shape(Y_Train))
+    print("the shape of Y_Train is  ",np.shape(Y_Train))
     print([np.max(X_Train_Normal[:,:,i]) for i in range(7)])
     print([np.min(X_Train_Normal[:,:,i]) for i in range(7)])
     print("the shape of X_Test is  ",np.shape(X_Test))
     print("the shape of normalized X_Test is  ",np.shape(X_Test_Normal))
-	print("the shape of Y_test is  ",np.shape(Y_Test))
+    print("the shape of Y_test is  ",np.shape(Y_Test))
     print([np.max(X_Test_Normal[:,:,i]) for i in range(7)])
     print([np.min(X_Test_Normal[:,:,i]) for i in range(7)])
 
-	#X_Train_Normal=X_Train
-	#X_Test_Normal=X_Test
 
-	# Training model 
+    # Training model 
 
-	model_unet.Training_Model(Train_model=train_model, Load_weights=Load_weights, validation_split=validation_split, batch_size = batch_size, epochs=epochs,
-		X_Train_Normal=X_Train_Normal,Y_Train=Y_Train,X_Test_Normal=X_Test_Normal,Y_Test=Y_Test,INPUT_WIDTH =INPUT_WIDTH, 
-		INPUT_HEIGHT = INPUT_HEIGHT,INPUT_CHANNELS = INPUT_CHANNELS,EVALUATION_PATH=EVALUATION_PATH)
+    model_unet.Training_Model(Train_model=train_model, Load_weights=Load_weights, validation_split=validation_split, batch_size = batch_size, epochs=epochs,
+        X_Train_Normal=X_Train_Normal,Y_Train=Y_Train,X_Test_Normal=X_Test_Normal,Y_Test=Y_Test,INPUT_WIDTH =INPUT_WIDTH, 
+        INPUT_HEIGHT = INPUT_HEIGHT,INPUT_CHANNELS = INPUT_CHANNELS,EVALUATION_PATH=EVALUATION_PATH)
 
 
 # Loading the weights model U-NET if it exists (True or False)
@@ -99,6 +104,6 @@ train_model=True
 mainPath=os.getcwd()
 
 Training_Unet(mainPath=mainPath,Augmentation=True,nb_Augmentation=3,INPUT_WIDTH = 96,INPUT_HEIGHT = 96, INPUT_CHANNELS = 7, OUTPUT_CHANNELS=1, 
-	Normalization ="Normalisation_by_image_by_column",Train_model=train_model, Load_weights=Load_weights,validation_split=0.1, batch_size = 20, epochs=30)
+    Normalization ="Normalisation_by_image_by_column",Train_model=train_model, Load_weights=Load_weights,validation_split=0.1, batch_size = 20, epochs=30)
 
 