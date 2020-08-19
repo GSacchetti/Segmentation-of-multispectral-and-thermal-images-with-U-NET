0e018ba6df644d19c32baad7e2991cd4b98db260
GSacchetti <48059787+GSacchetti@users.noreply.github.com>
2020-08-19 14:53:59 +0200

    Update load_train_test_data.py

--- load_train_test_data.py
+++ load_train_test_data.py
@@ -15,9 +15,10 @@ start_time = time.time()
 
 
 # mainPath : the path or the way to our training and testing data set.
-# size_image_Input=[96,96,7]([256,256,7]),size_mask_Output=[96,96,1]([256,256,1]: the sizes of images and masks. 
+# size_image=[96,96]([256,256]) : the sizes of images.
+#input_channels : [0,1,2,3,4,5,6,7] channels taken into account for training (7 channels by default).
 # augment=True, nb_augment=3 : augment: it is a question of whether or not we increase our data set and 
-#     "nb_augment = 3" is for the number of rotations of 90 Ãƒâ€šÃ‚Â°.
+#     "nb_augment = 3" is for the number of rotations of 90 degrees.
 # There are packages and functions that can generate them otherwise. However, this increase has greatly 
 #      improved our prediction results.
 
@@ -25,8 +26,7 @@ def mymkdir(path):
 	if not os.path.exists(path):
 		os.mkdir(path)
 		
-def load_training_test_data(mainPath='', size_image_Input=[96,96,7],
-    size_mask_Output=[96,96,1],augment=True, nb_augment=3): 
+def load_training_test_data(mainPath='', size_image=[256,256], input_channels = [0,1,2,3,4,5,6], augment=True, nb_augment=3): 
 
     #loading of Input and Output data
     #Initialization parameters and vetors training images and maskes set
@@ -59,8 +59,8 @@ def load_training_test_data(mainPath='', size_image_Input=[96,96,7],
 
     Test_Batch_size=np.size(list_Input_test)
 
-    Size_Input_train=[Train_Batch_size,size_image_Input[0],size_image_Input[1],size_image_Input[2]]
-    Size_Output_train=[Train_Batch_size,size_mask_Output[0],size_mask_Output[1],size_mask_Output[2]]
+    Size_Input_train=[Train_Batch_size,size_image[0],size_image[1],len(input_channels)]
+    Size_Output_train=[Train_Batch_size,size_image[0],size_image[1],1]
 
     X_Train=np.zeros(Size_Input_train, dtype=np.float32) 
     Y_Train=np.zeros(Size_Output_train, dtype=np.uint8) 
@@ -104,19 +104,18 @@ def load_training_test_data(mainPath='', size_image_Input=[96,96,7],
     if augment==True:
         list_input_dir.append('TrainNPY/Augment_images')
     list_Input=[os.path.join(PathTT,d,p) for d in list_input_dir for p in sorted(os.listdir(os.path.join(PathTT,d)))]
-    for nb_Input, Input in enumerate(list_Input):
+    print('Shape X_Train before loop : ', X_Train.shape)
+    print('size list_Input :', len(list_Input))
+    for nb_input,Input in enumerate(list_Input):
         InputArray=np.load(Input)
-        X_Train[nb_Input]=InputArray
+        X_Train[nb_input,:,:,:] = InputArray[:,:,input_channels]
         if 'Augment' in Input:
             g_images.write(str(Input)+'\n')    
         else:
             f_images.write(str(Input)+'\n')
-        #name='Image'+str(nb_Input)
-        #PathVerif=join(VERIFICATIONS_PATH,name)
-        #mymkdir(PathVerif)
-        #for i in range (InputArray.shape[2]):
-        #    filename_verif=join(PathVerif,'input'+str(nb_Input)+'ch'+str(i)+'.png')
-        #    imsave(filename_verif,InputArray[:,:,i].astype(np.uint8))
+    print('Training image has the following shape :' + str(X_Train.shape))
+    #assert X_Train.shape == (Test_Batch_size,96,96,len(input_channels)),'X_train has shape'+str(X_Train.shape)+' whereas input has shape '+ str((Test_Batc_Size,size_image[0],size_image[1],len(input_channels)))
+    print('Loading of training images completed')
 
     list_output_dirs = ['TrainNPY/masks/']
     if augment==True:
@@ -124,7 +123,7 @@ def load_training_test_data(mainPath='', size_image_Input=[96,96,7],
     list_Output=[os.path.join(PathTT,d,p) for d in list_output_dirs for p in sorted(os.listdir(os.path.join(PathTT,d)))]
 
     for nb_Output, Output in enumerate(list_Output):
-        OutputArray=np.load(Output)
+        OutputArray=np.load(Output,allow_pickle=True)
         Y_Train[nb_Output]=OutputArray
         if 'Augment' in Output:
             g_masks.write(str(Output)+'\n')    
@@ -132,18 +131,26 @@ def load_training_test_data(mainPath='', size_image_Input=[96,96,7],
             f_masks.write(str(Output)+'\n')
         #filename_verif=join(join(VERIFICATIONS_PATH,'Image'+str(nb_Input)),'output'+str(nb_Output)+'.png')
         #imsave(filename_verif,OutputArray.astype(np.uint8))
+    print('Training mask has the following shape :' + str(Y_Train.shape))
+    print('Loading of training masks completed')
 
     #Initializing parameters of input and output testing vector
-    size_Input_test=[Test_Batch_size,size_image_Input[0],size_image_Input[1],size_image_Input[2]]
-    size_Output_test=[Test_Batch_size,size_mask_Output[0],size_mask_Output[1],size_mask_Output[2]]
+    size_Input_test=[Test_Batch_size,size_image[0],size_image[1],len(input_channels)]
+    size_Output_test=[Test_Batch_size,size_image[0],size_image[1],1]
     X_Test=np.zeros(size_Input_test, dtype=np.float32) 
     Y_Test=np.zeros(size_Output_test, dtype=np.uint8) 
-
-    for nb_Input, Input in enumerate(sorted(os.listdir(PathTT+'TestNPY/images'))):
-        X_Test[nb_Input]=np.load(PathTT+'TestNPY/images/'+str(Input))
+    
+    for nb_input, Input in enumerate(sorted(os.listdir(PathTT+'TestNPY/images'))):
+        Input_Array_Test = np.load(PathTT+'TestNPY/images/'+str(Input))
+        X_Test[nb_input,:,:,:] = Input_Array_Test[:,:,input_channels]
+        #assert X_Test.shape == (Test_Batch_size,size_image[0],size_image[1],len(input_channels)), 'X_Test has shape '+str(X_Train.shape)+' whereas image has shape (Test_Batch_size,size_image[0],size_image[1],len(input_channels))'
+    print('Testing image has the following shape :' + str(X_Test.shape))
+    print('Loading of testings images completed')  
 
     for nb_Output, Output in enumerate(sorted(os.listdir(PathTT+'TestNPY/masks'))):
         Y_Test[nb_Output]=np.load(PathTT+'TestNPY/masks/'+Output)
+    print('Testing mask has the following shape :' + str(Y_Test.shape))
+    print('Loading of testing masks completed')
 
     f_images.close()
     f_masks.close()
